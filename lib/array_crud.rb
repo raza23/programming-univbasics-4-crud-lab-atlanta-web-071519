b0476bc0aed9a456af8fb7c3937fc70e7378604c
Raza Tahir Shareef <rtshareef@gmail.com>
2019-06-24 14:16:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,49 @@
 def create_an_empty_array
+nba = []
   
 end
 
 def create_an_array
+nba = ["Heat","Jazz"]
   
 end
 
 def add_element_to_end_of_array(array, element)
+nba.push("Cavs")
   
 end
 
 def add_element_to_start_of_array(array, element)
+nba.unshift("Monkeys")
   
 end
 
 def remove_element_from_end_of_array(array)
+nba.pop
   
 end
 
 def remove_element_from_start_of_array(array)
+nba.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
+nba[0]
   
 end
 
 def retrieve_first_element_from_array(array)
+nba[0]
   
 end
 
 def retrieve_last_element_from_array(array)
+nba[-1]
   
 end
 
 def update_element_from_index(array, index_number, element)
+nba[0]= "Magic"
 
 end
